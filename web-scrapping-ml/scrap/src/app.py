7e58fb3c85d0c39cbb1f4fc199afd3a125ea7411
binhojulix <fabio.jdluz@gmail.com>
2023-01-01 21:03:55 -0300

    docker

--- web-scrapping-ml/scrap/src/app.py
+++ web-scrapping-ml/scrap/src/app.py
@@ -2,6 +2,6 @@ from scrap import Scrap
 
 
 if __name__ == "__main__":
+   
    scrap = Scrap(5)
    scrap.start()
-  
--- web-scrapping-ml/scrap/src/scrap.py
+++ web-scrapping-ml/scrap/src/scrap.py
@@ -37,58 +37,59 @@ class Scrap(Thread):
             try:
                 response = urlopen(url)
                 logging.info("Requisicao..." + url)
-            except:
-                logging.error('URL INVALIDA!')
+                html = response.read().decode('utf-8')
     
 
-            html = response.read().decode('utf-8')
-            soup = BeautifulSoup(html, 'html.parser')
-            ol = soup.find_all('ol', {'class': ['ui-search-layout', 'ui-search-layout--stack']})
-        
-            for li in ol:
-                for li in li.findAll('li', {'class':['ui-search-layout__item']}):
-                
-                    
-                    produtoBuilder = ProdutoBuilder() 
-                
-                    titulo = li.h2.getText()
-                    produtoBuilder.com_titulo(titulo)
-                        
-                    amounts = li.findAll('span', {'class':['ui-search-reviews__amount']})
-                    for amount in amounts:
-                        if(not amount.getText()):
-                            produtoBuilder.com_reviews_amount(0)
-                        else:
-                            produtoBuilder.com_reviews_amount(amount.getText())
-                    
-                    preco = li.findAll('span', {'class': ['price-tag-fraction']})[0].getText()
-                    produtoBuilder.com_preco(preco)
-                    
-                    fretes = li.findAll('p', {'class': ['ui-search-item__shipping', 'ui-search-item__shipping--free']})
-                    for frete in fretes:
-                        if(not frete.getText()):
-                            produtoBuilder.com_frete(0)
-                        else:
-                            produtoBuilder.com_frete(frete.getText())
-                        
-                    cores = li.findAll('span', {'class': ['ui-search-item__group__element', 'ui-search-item__variations-text']})
-                    for cor in cores:
-                        if(not cor.getText()):
-                            produtoBuilder.com_cores_disponiveis(0)
-                        else:
-                            produtoBuilder.com_cores_disponiveis(cor.getText())
 
-                    produto = produtoBuilder.build()
-                    produtos.append(produto)
-                 
-        df = pd.DataFrame([t.__dict__ for t in produtos ])
-        df.rename(columns={'_Produto__reviews_amount': 'reviews_amount', '_Produto__cores_disponiveis': 'cores_disponiveis',
-                  '_Produto__frete':'produto_frete', '_Produto__titulo':'produto_titulo', 
-                   'produto__titulo':'Produto_titulo', '_Produto__preco':'produto_preco'}, inplace=True)
-        textStract = TextStract()
-        df['produto_preco'] = df['produto_preco'].apply(textStract.convertDouble)
-        df.to_csv('dataset.csv', sep=';', index = False, encoding = 'utf-8-sig')
-        
+                soup = BeautifulSoup(html, 'html.parser')
+                ol = soup.find_all('ol', {'class': ['ui-search-layout', 'ui-search-layout--stack']})
+            
+                for li in ol:
+                    for li in li.findAll('li', {'class':['ui-search-layout__item']}):
+                    
+                        
+                        produtoBuilder = ProdutoBuilder() 
+                    
+                        titulo = li.h2.getText()
+                        produtoBuilder.com_titulo(titulo)
+                            
+                        amounts = li.findAll('span', {'class':['ui-search-reviews__amount']})
+                        for amount in amounts:
+                            if(not amount.getText()):
+                                produtoBuilder.com_reviews_amount(0)
+                            else:
+                                produtoBuilder.com_reviews_amount(amount.getText())
+                        
+                        preco = li.findAll('span', {'class': ['price-tag-fraction']})[0].getText()
+                        produtoBuilder.com_preco(preco)
+                        
+                        fretes = li.findAll('p', {'class': ['ui-search-item__shipping', 'ui-search-item__shipping--free']})
+                        for frete in fretes:
+                            if(not frete.getText()):
+                                produtoBuilder.com_frete(0)
+                            else:
+                                produtoBuilder.com_frete(frete.getText())
+                            
+                        cores = li.findAll('span', {'class': ['ui-search-item__group__element', 'ui-search-item__variations-text']})
+                        for cor in cores:
+                            if(not cor.getText()):
+                                produtoBuilder.com_cores_disponiveis(0)
+                            else:
+                                produtoBuilder.com_cores_disponiveis(cor.getText())
+
+                        produto = produtoBuilder.build()
+                        produtos.append(produto)
+                    
+                df = pd.DataFrame([t.__dict__ for t in produtos ])
+                df.rename(columns={'_Produto__reviews_amount': 'reviews_amount', '_Produto__cores_disponiveis': 'cores_disponiveis',
+                        '_Produto__frete':'produto_frete', '_Produto__titulo':'produto_titulo', 
+                        'produto__titulo':'Produto_titulo', '_Produto__preco':'produto_preco'}, inplace=True)
+                textStract = TextStract()
+                df['produto_preco'] = df['produto_preco'].apply(textStract.convertDouble)
+                df.to_csv('dataset.csv', sep=';', index = False, encoding = 'utf-8-sig')
+            except:
+                logging.error('URL INVALIDA!')
+
    
 
     def run(self):
